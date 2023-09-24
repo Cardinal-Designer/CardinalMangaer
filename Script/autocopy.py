abddb297524f9f39cce6cf654c1828e69b6a223d
HenryZeng <zengdeveloper@qq.com>
2023-09-24 20:06:49 +0800

    修改搜索算法 (bfs) + 修改为广度优先算法，将多次查询合并，减少利用管道创建子进程带来的延时，加速搜索速度。

--- Script/autocopy.py
+++ Script/autocopy.py
@@ -8,11 +8,20 @@ shield_path = [
 
 dependeces = []
 
-def search_dependeces(msys2_target : str):
-  with Popen(["ntldd",msys2_target],stdout=PIPE) as p:
+dep_tmp = []
+
+def search_dependeces(msys2_target : list):
+
+  with Popen(["ntldd"] + msys2_target,stdout=PIPE) as p:
     dep_raw = p.stdout.read()
 
-  dep_raw = dep_raw.decode(encoding="utf-8")
+  dep_raw = dep_raw.decode(encoding="utf-8") # byte 数据转换到str，方便处理
+
+  for i in msys2_target:
+    if i in dep_raw:
+      dep_raw = dep_raw.replace(i + ":","")
+
+
   dep_raw = dep_raw.replace("\r","")
   dep_raw = dep_raw.replace("\t","")
   dep_raw = dep_raw.split("\n")
@@ -39,9 +48,24 @@ def search_dependeces(msys2_target : str):
           continue
 
         print(processed)
-        dependeces.append(processed)
+        dep_tmp.append(processed)
 
-        search_dependeces(processed[1]) # 递归搜索
+def search_dependeces_bfs(msys2_target : list):
+  global dep_tmp
+  global dependeces
+  # bfs : 广度优先搜索
+
+  search_dependeces(msys2_target)
+
+  target_tmp = []
+  while dep_tmp != []: # 只要还有搜索结果，就继续搜索
+    for i in dep_tmp:
+      target_tmp.append(i[1]) # 将dll路径汇总
+      dependeces = dependeces + dep_tmp # 将上一层搜索结果合并到主线
+      dep_tmp = [] # 清除上一次搜索结果
+
+      search_dependeces(target_tmp) # 开始下一层搜索
+      target_tmp = [] # 搜索结束，清除临时路径列表
 
 def copy_dep(target_path:str):
   for i in dependeces:
@@ -49,10 +73,11 @@ def copy_dep(target_path:str):
       pass
 
 
+
 if __name__ == "__main__":
   if len(sys.argv) < 3:
     print("[Error] 参数过少")
     exit
 
-  search_dependeces(sys.argv[1])
+  search_dependeces_bfs([sys.argv[1]])
   copy_dep(sys.argv[2])
